8044e2cdd200ce88d3d266167f9a14f09c7b6313
Ariq Farhan <122168324+AriqF1@users.noreply.github.com>
2025-07-03 11:52:16 +0700

    feat: implement DashboardQuiz component for quiz management with dynamic question handling (#5)

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,15 +1,8 @@
 import React from "react";
 
 function Button({ children, type = "button", className = "", onClick }) {
-  const baseStyle =
-    "w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
-
   return (
-    <button
-      type={type}
-      className={`${baseStyle} ${className}`}
-      onClick={onClick}
-    >
+    <button type={type} className={`${className}`} onClick={onClick}>
       {children}
     </button>
   );
